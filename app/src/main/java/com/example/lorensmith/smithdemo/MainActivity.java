2d212c0c1317e9f138ced434f11e03d2c257b8ab
Loren Smith <lorencsmith@comcast.net>
2017-01-30 18:41:18 -0500

    in-class activitiy changes 1/30/17

--- app/src/main/java/com/example/lorensmith/smithdemo/MainActivity.java
+++ app/src/main/java/com/example/lorensmith/smithdemo/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private ImageButton bt1;
+    private ImageButton bt2;
     private ImageButton bt3;
 
     @Override
@@ -22,13 +23,21 @@ public class MainActivity extends AppCompatActivity {
     }
     private void initialView(){
         bt1 = (ImageButton) findViewById(R.id.bt1);
+        bt2 = (ImageButton) findViewById(R.id.bt2);
         bt3 = (ImageButton) findViewById(R.id.bt3);
     }
+
     private void intialListener(){
         bt1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Toast.makeText(v.getContext(),"Button1 was clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+        bt2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(v.getContext(),"Button2 was clicked", Toast.LENGTH_LONG).show();
             }
         });
         bt3.setOnClickListener(new View.OnClickListener(){
